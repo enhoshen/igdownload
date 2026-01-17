113def5d02f1f3a81dba8a9c58bc0aa617bff42a
enhoshen <enhoshen@gmail.com>
2026-01-17 19:12:38 +0800

    feat: Add collection parsing using instagrapi
    * l,login: to login using sessionid string (copy from browser)
    * c,collection: collection name
    * download_links: file path to put links in
    * unsave: whether to unsave the media after parsed

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 import argparse
+import os
 import re
 import instaloader
 import logging
+import instagrapi
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +13,21 @@ L = instaloader.Instaloader()
 # L.interactive_login(USER)  # (ask password on terminal)
 # L.load_session_from_file(USER)  # (load session created w/
 #  `instaloader -l USERNAME`)
+
+
+def parse_url(url: str, loader: instaloader.Instaloader):
+    url = url.strip()  # Remove leading/trailing whitespace
+    post_shortcode = re.search(r"/p/(.*)/", url)
+    if post_shortcode is not None:
+        try:
+            post = instaloader.Post.from_shortcode(L.context, post_shortcode[1])
+            L.download_post(post, target=post_shortcode[1])
+            logger.info(f"Downloaded successfully from {url}")
+        except Exception as e:
+            logger.error(f"Error downloading from {url}: {e}")
+            error_url.append(url)
+            error.write(url + "\n")
+            error.flush()
 
 
 if __name__ == "__main__":
@@ -33,16 +50,33 @@ if __name__ == "__main__":
         help="The directory to save the media to.",
     )
     parser.add_argument(
+        "-l",
+        "--login",
+        help="Account login, followed by session id",
+    )
+    parser.add_argument(
+        "-c",
+        "--collection",
+        default="download",
+        help="The collection to download links",
+    )
+    parser.add_argument(
+        "--unsave",
+        action="store_true",
+        help="After operation done, unsave the media",
+    )
+    # collection operations
+    parser.add_argument(
+        "--download_links",
+        default="links.txt",
+        help="Download media links to a file",
+    )
+    parser.add_argument(
         "--log_level",
         dest="log_level",
         default="INFO",
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
         help="Set the logging level",
-    )
-    parser.add_argument(
-        "-c",
-        "--cookies",
-        help="Path to a JSON file containing cookies for authentication.",
     )
     args = parser.parse_args()
     logger.setLevel(args.log_level)
@@ -50,6 +84,12 @@ if __name__ == "__main__":
     error_url = []
     L.dirname_pattern = f"{args.output}/{{target}}"
     L.filename_pattern = f"{{target}}-{{date_utc}}"
+
+    client = None
+    if args.login:
+        client = instagrapi.Client()
+        client.login_by_sessionid(args.login)
+
     if args.url:
         post_shortcode = re.search(r"/p/(.*)/?", args.url)
         if post_shortcode is not None:
@@ -67,17 +107,23 @@ if __name__ == "__main__":
     if args.input:
         with open(args.input, "r") as inpt, open("error.txt", "a+") as error:
             for url in inpt:
-                url = url.strip()  # Remove leading/trailing whitespace
-                post_shortcode = re.search(r"/p/(.*)/", url)
-                if post_shortcode is not None:
-                    try:
-                        post = instaloader.Post.from_shortcode(
-                            L.context, post_shortcode[1]
-                        )
-                        L.download_post(post, target=post_shortcode[1])
-                        logger.info(f"Downloaded successfully from {url}")
-                    except Exception as e:
-                        logger.error(f"Error downloading from {url}: {e}")
-                        error_url.append(url)
-                        error.write(url + "\n")
-                        error.flush()
+                parse_url(url=url, loader=L)
+
+    # login required
+    if client is None:
+        exit(0)
+
+    if args.collection:
+        collection_pk = client.collection_pk_by_name(args.collection)
+
+    if args.download_links:
+        medias = client.collection_medias_by_name(args.collection)
+        with open(args.download_links, "w") as links, open(
+            "error.txt", "a+"
+        ) as error:
+            for m in medias:
+                links.write(f"https://www.instagram.com/p/{m.code}/\n")
+                if args.unsave:
+                    client.media_unsave(
+                        media_id=m.id, collection_pk=collection_pk
+                    )
